ab3ac8cceb940b030e6c768680344a5c437236a5
MindBringer <jan.jansen@outlook.de>
2025-07-11 13:46:31 +0200

    1

--- docker/frontend/src/components/Ask.tsx
+++ docker/frontend/src/components/Ask.tsx
@@ -24,6 +24,7 @@ export default function Ask() {
     } catch (err) {
       setAnswer("Fehler: " + (err as Error).message);
     }
+  };
 
   return (
     <div className="max-w-xl mx-auto mt-8 p-4 border rounded-xl shadow">
